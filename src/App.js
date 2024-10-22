92a25b63d51c80a4e59f06d3171a704a8f698dc6
Lizyimin-cpu <111258834+Lizyimin-cpu@users.noreply.github.com>
2024-10-22 12:28:05 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Contact from "./components/Contact";
 import About from "./components/About";
-import Menu from "./components/Menu"; // 你可能有的现有菜单页面
-import MenuCard from "./components/MenuCard"; // 这是我们要跳转到的菜单卡片页面
-import OpeningHours from "./components/OpeningHours"; // 这是我们要显示营业时间的页面
+import Menu from "./components/Menu";
+import MenuCard from "./components/MenuCard"; 
+import OpeningHours from "./components/OpeningHours"; 
 import "./App.css";
 
 function App() {
@@ -19,9 +19,9 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
         <Route path="/menu-card" element={<MenuCard />} />{" "}
-        {/* 确保这个路径是你跳转的目标 */}
+        
         <Route path="/opening-hours" element={<OpeningHours />} />{" "}
-        {/* 确保这是营业时间页面的正确路径 */}
+       
       </Routes>
     </div>
   );
